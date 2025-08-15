a16cc85a31ea744c2f44223239ed587cfdb02521
villno55 <adsotrabajos24@gmail.com>
2025-08-15 18:54:20 -0500

    ejercicio2.js

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,6 +1,7 @@
 
 function contarVocales(texto) {
-  return (texto.match(/[aeiouáéíóú]/gi) || []).length;
+  return (texto.match(/[aeiouáéíóú]/gi) || []).length; //Se usa g e i para q tenga encuentra tanto mayúsculas y minusculas
 }
 
 console.log(contarVocales("Hola Mundo"));
+
