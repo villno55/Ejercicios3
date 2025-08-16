cdaabe6ca2fa87e19811680677f5bf4fbc6d69ba
villno55 <adsotrabajos24@gmail.com>
2025-08-15 21:05:46 -0500

    Update

---
+++ ejercicio10.js
@@ -0,0 +1,17 @@
+const prompt = require("prompt-sync")();
+
+
+function cambiarValor(lista, viejo, nuevo) {
+  for (let i = 0; i < lista.length; i++) {
+    if (lista[i] === viejo) {
+      lista[i] = nuevo;
+    }
+  }
+  return lista;
+}
+
+let entrada = prompt("Ingrese elementos separados por coma: ");
+let lista = entrada.split(",");
+let viejo = prompt("Valor a reemplazar: ");
+let nuevo = prompt("Nuevo valor: ");
+console.log("Resultado:", cambiarValor(lista, viejo, nuevo));
