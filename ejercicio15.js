451c98cb40a31e897eb59ab16298d77450714a83
villno55 <adsotrabajos24@gmail.com>
2025-08-15 21:27:21 -0500

    Cambio funcion3v.c

---
+++ ejercicio15.js
@@ -0,0 +1,59 @@
+const prompt = require("prompt-sync")();
+
+let cuentas = [];
+let numCuenta = 1;
+
+function crearCuenta() {
+  let anio = new Date().getFullYear();
+  let codigo = anio + "-" + numCuenta;
+  numCuenta++;
+
+  let cuenta = {
+    codigo: codigo,
+    saldo: 0
+  };
+
+  cuentas.push(cuenta);
+  return cuenta;
+}
+
+function consignar(cod, plata) {
+  for (let c of cuentas) {
+    if (c.codigo === cod) {
+      c.saldo = c.saldo + plata;
+    }
+  }
+}
+
+function retirar(cod, plata) {
+  for (let c of cuentas) {
+    if (c.codigo === cod && c.saldo >= plata) {
+      c.saldo = c.saldo - plata;
+    }
+  }
+}
+
+function verCuenta(cod) {
+  for (let c of cuentas) {
+    if (c.codigo === cod) {
+      return c;
+    }
+  }
+  return "no existe";
+}
+
+function verTodo() {
+  return cuentas;
+}
+
+// uso
+let c1 = crearCuenta();
+console.log("Se creó cuenta:", c1);
+
+let monto = parseInt(prompt("Cuánto mete a la cuenta?: "));
+consignar(c1.codigo, monto);
+
+let saca = parseInt(prompt("Cuánto saca?: "));
+retirar(c1.codigo, saca);
+
+console.log("Cuenta final:", verCuenta(c1.codigo));
