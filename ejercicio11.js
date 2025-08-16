57dceb7a95b86f3fe964ce6876f9d41493b1e2a9
villno55 <adsotrabajos24@gmail.com>
2025-08-15 21:15:13 -0500

    Update ejercicio11.js

---
+++ ejercicio11.js
@@ -0,0 +1,21 @@
+const prompt = require("prompt-sync")();
+
+
+function buscarCapital(pais) {
+  let listaPaises = new Map([
+    ["China","pekin"],
+    ["honduras","tegucigalpa"],
+    ["rusia","moscu"],
+    ["japon","tokio"],
+    ["Perú","Lima"],
+    ["brazil","brasilia"],
+    ["portugal","lisboa"],
+    ["India","new delhi"],
+    ["Alemania","Berlín"],
+    ["canada","toronto"]
+  ]);
+  return listaPaises.get(pais) || "País no registrado";
+}
+
+let pais = prompt("Escriba un país: ");
+console.log("Capital:", buscarCapital(pais));
